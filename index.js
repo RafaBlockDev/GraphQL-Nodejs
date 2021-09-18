aae0b66dbced94d82629092f94e995c983188e9c
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-17 22:55:23 -0500

    graphql app

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const app = express();
 
 const express_graphql = require("express-graphql");
+const{ buildSchema } = require("graphql");
+
+app.use("/graphql", );
 
 app.listen(3000, () => console.log("server on port 3000"))
